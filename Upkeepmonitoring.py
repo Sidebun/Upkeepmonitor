16437aa0b8c6f0bf3ff2c1388544edb3800f9970
Sidebun <Soren.sadegi@live.se>
2021-02-15 00:05:11 +0100

    Update Upkeepmonitoring.py

--- Upkeepmonitoring.py
+++ Upkeepmonitoring.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 
 r = tk.Tk() 
-r.title('Counting Seconds') 
+r.title('Upkeep monitor') 
+r.geometry("800x600")
+r.configure(bg="blue")
 button = tk.Button(r, text='Stop', width=25, command=r.destroy) 
 button.pack() 
 r.mainloop() 
 
 
-#hejhej
 
 
 
