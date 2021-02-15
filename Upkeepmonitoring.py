e2397ed9eced1965510967aed75090852b6f7b4c
Sidebun <Soren.sadegi@live.se>
2021-02-15 01:48:53 +0100

    first canvases and iconbitmap

--- Upkeepmonitoring.py
+++ Upkeepmonitoring.py
@@ -1,11 +1,19 @@
 import tkinter as tk
+from tkinter import *
 
 r = tk.Tk() 
-r.title('Upkeep monitor') 
+r.title('Upkeepmonitor') 
 r.geometry("800x600")
 r.configure(bg="blue")
-button = tk.Button(r, text='Stop', width=25, command=r.destroy) 
-button.pack() 
+r.iconbitmap("icon.ico")
+
+networkname = tk.Canvas(r,width=300,height=50,bg="white")
+
+networkname.place(relx=0.5,rely=0.1,anchor=CENTER)
+
+rectangle1 = tk.Canvas(r, width=150,height=400, bg="white")
+rectangle1.place(relx=0.1,rely=0.3)
+
 r.mainloop() 
 
 
