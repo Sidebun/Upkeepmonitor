dd87e12f42b5df6d75d196c6d68a5d57404510c9
Sidebun <Soren.sadegi@live.se>
2021-02-15 18:06:23 +0100

    Update Upkeepmonitoring.py

--- Upkeepmonitoring.py
+++ Upkeepmonitoring.py
@@ -1,5 +1,15 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
+import requests
+import threading
+from threading import Thread
+
+
+
+
+
+
 
 r = tk.Tk() 
 r.title('Upkeepmonitor') 
@@ -14,44 +24,77 @@ networkname.place(relx=0.5,rely=0.1,anchor=CENTER) #Set position relative to scr
 networkname.insert(tk.END,NameOfNetwork) #Insert the text "NameOfNetwork"
 
 networkname.config(state='disabled') #Disable input/editing of networkname text.
-#-------------------------------------------------------------------------------------------------#
-
-#NameBarForDevices = tk.Text(r,width=62,height=1,bg="blue",fg="white")
-#NameBarForDevices.config(font=("Courier",20)) #Set font & size of text
-#NameBarForDevices.place(relx=0.5,rely=0.21,anchor=CENTER)
-
+#------------------------------------------------------------------#
 #Devices will be shown here with all information for it.
 DeviceCanvas = tk.Canvas(r, width=1000,height=650, bg="#626262")
 
+#------------------------------------------------------------------#
+button1 = Button(r, text = "Devices",command=r.quit, anchor = W,bg="white")
+button1.configure(width =12,font=("MS Sans Serif",20),relief=RAISED,borderwidth=5)
+button1_window = DeviceCanvas.create_window(1, 0, anchor=NW, window=button1)
 
-button1 = Button(r, text = "Devices", anchor = W,bg="white")
-button1.configure(width =10,font=("MS Sans Serif",20),relief=RAISED,borderwidth=5)
-button1_window = DeviceCanvas.create_window(0, 0, anchor=NW, window=button1)
-
-
+#------------------------------------------------------------------#
 button2 = Button(r, text = "IP Address", anchor = W,bg="white")
-button2.configure(width =10,font=("MS Sans Serif",20),relief=RAISED,borderwidth=5)
-button2_window = DeviceCanvas.create_window(250, 0, anchor=NW, window=button2)
-
+button2.configure(width =12,font=("MS Sans Serif",20),relief=RAISED,borderwidth=5)
+button2_window = DeviceCanvas.create_window(300, 0, anchor=NW, window=button2)
+#------------------------------------------------------------------#
 button3 = Button(r, text = "Status", anchor = W,bg="white")
 button3.configure(width =10,font=("MS Sans Serif",20),relief=RAISED,borderwidth=5)
 button3_window = DeviceCanvas.create_window(800, 0, anchor=NW, window=button3)
+#------------------------------------------------------------------#
+#List Devices, Should be able to add dynamically with  lD.insert(DeviceNr,"NameOfDevice") or something like that.
+lD = Listbox(r)
+lD.insert(1,"Soren PC")
+lD.insert(2,"Duh")
+lD.insert(3,"Soren dsadsadsadsdsadPC")
+lD.insert(4,"Soren PC")
 
+lD.configure(width=15,height=20,font=("MS Sans Serif",15))
+lD.place(relx=0.18,rely=0.60,anchor=CENTER)
 
+#------------------------------------------------------------------#
+#When it adds device it will also insert the IP for the device, (lIP.insert(DeviceNr,"IP"))
+lIP = Listbox(r)
 
+lIP.insert(1,"192.168.0.1")
+lIP.insert(2,"192.168.1.1")
+lIP.insert(3,"127.0.0.1")
+lIP.insert(4,"10.0.0.1")
+
+lIP.configure(width=45,height=20,font=("MS Sans Serif",15))
+lIP.place(relx=0.5,rely=0.60,anchor=CENTER)
+#------------------------------------------------------------------#
+
+lStatus = Listbox(r)
+
+lStatus.insert(1,"192.168.0.1")
+lStatus.insert(2,"192.168.1.1")
+lStatus.insert(3,"127.0.0.1")
+lStatus.insert(4,"10.0.0.1")
+
+lStatus.configure(width=45,height=20,font=("MS Sans Serif",15))
+lStatus.place(relx=0.7,rely=0.60,anchor=CENTER)
 
 DeviceCanvas.place(relx=0.5,rely=0.6,anchor=CENTER)
 
-
-r.resizable(False, False) 
+r.resizable(False, False)
 r.mainloop() 
 
 
 
 
+def connected():
+    host="http://google.com"
+    r.after(2000,connected)
+    try:
+        if requests.get(host).ok:
+            print("You're Online")
+    except:
+        print("You're Offline")
 
 
-
+x = threading.Thread(target=connected,daemon=True)
+x.start()
 
 
 
